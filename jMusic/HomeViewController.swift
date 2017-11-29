714efd829f322b5181b4abae09dbe8ad0b359d5e
Jeevan <jeevaneashwar2@gmail.com>
2017-11-29 17:21:53 +0530

    online songs with http, songlabel realign,Migration from AVAudioPlayer to AVPlayer

--- jMusic/HomeViewController.swift
+++ jMusic/HomeViewController.swift
@@ -10,7 +10,6 @@ import UIKit
 import AVFoundation
 import MediaPlayer
 extension UILabel {
-    
     func countLabelLines() -> Int {
         // Call self.layoutIfNeeded() if your view is uses auto layout
         let myText = self.text! as NSString
@@ -19,7 +18,6 @@ extension UILabel {
         let labelSize = myText.boundingRect(with: CGSize(width: self.bounds.width, height: CGFloat.greatestFiniteMagnitude), options: NSStringDrawingOptions.usesLineFragmentOrigin, attributes: attributes, context: nil)
         return Int(ceil(CGFloat(labelSize.height) / self.font.lineHeight))
     }
-    
     func isTruncated() -> Bool {
         
         if (self.countLabelLines() > self.numberOfLines) {
@@ -28,9 +26,20 @@ extension UILabel {
         return false
     }
 }
+extension AVPlayer {
+    var isReadyToPlay:Bool {
+        let timeRange = currentItem?.loadedTimeRanges.first as? CMTimeRange
+        guard let duration = timeRange?.duration else { return false }
+        let timeLoaded = Int(duration.value) / Int(duration.timescale) // value/timescale = seconds
+        let loaded = timeLoaded > 0
+        
+        return status == .readyToPlay && loaded
+    }
+}
 class HomeViewController: UIViewController,AVAudioPlayerDelegate,UICollectionViewDataSource,UICollectionViewDelegate	 {
     
-    var audioPlayer: AVAudioPlayer?
+    var audioPlayer: AVPlayer?
+    var playerItem:AVPlayerItem?
     var currentPlayTime:TimeInterval?
     var timer = Timer()
     @IBOutlet weak var artistLabel: UILabel!
@@ -43,7 +52,7 @@ class HomeViewController: UIViewController,AVAudioPlayerDelegate,UICollectionVie
     @IBOutlet weak var songDurationLabel: UILabel!
     @IBOutlet weak var currentTimeLabel: UILabel!
     var songImage:UIImage!
-    // MARK: Methods
+    // MARK: VC Methods
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -55,120 +64,74 @@ class HomeViewController: UIViewController,AVAudioPlayerDelegate,UICollectionVie
         songsCollectionView.decelerationRate=UIScrollViewDecelerationRateFast
         let nib = UINib(nibName: "SongInfoCell", bundle: nil)
         songsCollectionView.register(nib, forCellWithReuseIdentifier: "songInfoCell")
-        let soundURL = NSURL(fileURLWithPath: Bundle.main.path(forResource: "ShapeOfYou", ofType: "mp3")!)
+        //let soundURL = NSURL(fileURLWithPath: Bundle.main.path(forResource: "ShapeOfYou", ofType: "mp3")!)
         //let audioInfo = MPNowPlayingInfoCenter.default()
         //var nowPlayingInfo:[NSObject:AnyObject] = [:]
         do {
-            audioPlayer = try AVAudioPlayer(contentsOf: soundURL as URL)
-            audioPlayer!.delegate = self
-            songDurationLabel.text = changeTimeIntervalToDisplayableString(time: audioPlayer!.duration)
-            progressIndicator.minimumValue=0.0
-            progressIndicator.maximumValue=Float(audioPlayer!.duration)
-            songNameLabel.text="Shape of you - Ed Shereen. Shape of you mp3"
-            songNameLabel.translatesAutoresizingMaskIntoConstraints = false
-            setupAutoLayout(label: songNameLabel)
-            if(songNameLabel.isTruncated()){
-                startMarqueeLabelAnimation(label: songNameLabel)
-            }
-            /****************************/
-            let playerItem = AVPlayerItem(url: soundURL as URL)  //this will be your audio source
-            print(soundURL.path!)
+            let url = NSURL(string: "http://s3.amazonaws.com/kargopolov/BlueCafe.mp3")
+            //let url = NSURL(string: "http://www.soundhelix.com/examples/mp3/SoundHelix-Song-1.mp3")
+            playerItem = AVPlayerItem(url: url! as URL)
+            audioPlayer = AVPlayer(playerItem: playerItem)
+            let currentItemDurationAsCMTime:CMTime = (audioPlayer!.currentItem?.asset.duration)!
             
-            let metadataList = playerItem.asset.metadata 
-            for item in metadataList {
-                if item.commonKey != nil && item.value != nil {
-                    if item.commonKey  == "title" {
-                        print(item.stringValue)
-                        songNameLabel.text = item.stringValue
-                    }
-                    if item.commonKey   == "type" {
-                        print(item.stringValue)
-                        //nowPlayingInfo[MPMediaItemPropertyGenre] = item.stringValue
-                    }
-                    if item.commonKey  == "albumName" {
-                        print(item.stringValue)
-                        //nowPlayingInfo[MPMediaItemPropertyAlbumTitle] = item.stringValue
-                    }
-                    if item.commonKey   == "artist" {
-                        print(item.stringValue)
-                        artistLabel.text = item.stringValue
-                    }
-                    if item.commonKey  == "artwork" {
-                        if let image = UIImage(data: (item.value as! NSData) as Data) {
-                            //nowPlayingInfo[MPMediaItemPropertyArtwork] = MPMediaItemArtwork(image: image)
-                            print(image.description)
-                            songImage = image
+            songDurationLabel.text = changeTimeIntervalToDisplayableString(time: currentItemDurationAsCMTime.seconds)
+            progressIndicator.minimumValue=0.0
+            progressIndicator.maximumValue=Float(currentItemDurationAsCMTime.seconds)
+            
+            /****************************/
+            
+            if let metadataList = playerItem?.asset.metadata{
+                for item in metadataList {
+                    if item.commonKey != nil && item.value != nil {
+                        if item.commonKey  == "title" {
+                            print(item.stringValue!)
+                            songNameLabel.text = item.stringValue!
+                            songNameLabel.translatesAutoresizingMaskIntoConstraints = false
+                            setupAutoLayout(label: songNameLabel)
+                            if(songNameLabel.isTruncated()){
+                                startMarqueeLabelAnimation(label: songNameLabel)
+                            }
+                        }
+                        if item.commonKey   == "type" {
+                            print(item.stringValue!)
+                            //nowPlayingInfo[MPMediaItemPropertyGenre] = item.stringValue
+                        }
+                        if item.commonKey  == "albumName" {
+                            print(item.stringValue!)
+                            //nowPlayingInfo[MPMediaItemPropertyAlbumTitle] = item.stringValue
+                        }
+                        if item.commonKey   == "artist" {
+                            print(item.stringValue!)
+                            artistLabel.text = item.stringValue
+                        }
+                        if item.commonKey  == "artwork" {
+                            if let image = UIImage(data: (item.value as! NSData) as Data) {
+                                //nowPlayingInfo[MPMediaItemPropertyArtwork] = MPMediaItemArtwork(image: image)
+                                print(image.description)
+                                songImage = image
+                            }
                         }
                     }
                 }
+                
             }
+            
         }
-        catch{   }
-        
     }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    func updateCurrentTime(){
-        let currentTime:TimeInterval=audioPlayer!.currentTime
-        currentTimeLabel.text=changeTimeIntervalToDisplayableString(time: currentTime)
-        progressIndicator.setValue(Float(currentTime), animated: false)
+    override func viewWillAppear(_ animated: Bool) {
+        NotificationCenter.default.addObserver(
+            self,
+            selector: #selector(self.finishedPlaying(myNotification:)),
+            name: NSNotification.Name.AVPlayerItemDidPlayToEndTime,
+            object: audioPlayer?.currentItem)
     }
-    func changeTimeIntervalToDisplayableString(time:TimeInterval)->String{
-        var minutes = floor(time/60)
-        var seconds = Int(round(time - minutes * 60))
-        if(seconds==60){
-            seconds=0
-            minutes=minutes+1
-        }
-        let stringMinutes:String
-        let stringSeconds:String
-        if(Int(minutes)<10){
-            stringMinutes="0\(Int(minutes))"
-        }
-        else{
-            stringMinutes="\(Int(minutes))"
-        }
-        if(seconds<10){
-            stringSeconds="0\(seconds)"
-        }
-        else{
-            stringSeconds="\(seconds)"
-        }
-        return "\(stringMinutes):\(stringSeconds)"
-    }
-    func startMarqueeLabelAnimation(label:UILabel) {
-        
-        DispatchQueue.main.async(execute: {
-            
-            UILabel.animate(withDuration: 10.0, delay: 0.0, options: ([.curveEaseOut, .repeat]), animations: {() -> Void in
-                label.frame.origin.x-=200
-                
-            }, completion:  nil)
-        })
-    }
-    func setupAutoLayout(label:UILabel) {
-        let horizontalConstraintLeft = NSLayoutConstraint(item: label, attribute: NSLayoutAttribute.leftMargin, relatedBy: NSLayoutRelation.equal, toItem: playerLayoutView, attribute: NSLayoutAttribute.leftMargin, multiplier: 1, constant: 20)
-        let horizontalConstraintRight = NSLayoutConstraint(item: label, attribute: NSLayoutAttribute.rightMargin, relatedBy: NSLayoutRelation.equal, toItem: playerLayoutView, attribute: NSLayoutAttribute.rightMargin, multiplier: 1, constant: 20)
-        let verticalConstraint = NSLayoutConstraint(item: label, attribute: NSLayoutAttribute.topMargin, relatedBy: NSLayoutRelation.equal, toItem: playerLayoutView, attribute: NSLayoutAttribute.topMargin, multiplier: 1, constant: 20)
-        
-        self.playerLayoutView.addConstraints([horizontalConstraintLeft,horizontalConstraintRight, verticalConstraint])
-        
-    }
-    func highLightCellAtIndexPath(indexPath:IndexPath){
-        songsCollectionView.scrollToItem(at: indexPath, at: .centeredHorizontally, animated: true)
-        for i in 0..<songsCollectionView.visibleCells.count {
-            let cell = songsCollectionView.visibleCells[i]
-            cell.layer.borderWidth=0.0
-            cell.alpha=0.3
-        }
-        let cell = songsCollectionView.cellForItem(at: indexPath)
-        cell?.layer.borderWidth = 2.0
-        cell?.layer.borderColor = UIColor.gray.cgColor
-        cell?.layer.cornerRadius=(cell?.layer.frame.width)!/2
-        cell?.alpha=1.0
+    override func viewWillDisappear(_ animated: Bool) {
+        NotificationCenter.default.removeObserver(self)
     }
     // MARK: CollectionView Methods
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
@@ -220,31 +183,45 @@ class HomeViewController: UIViewController,AVAudioPlayerDelegate,UICollectionVie
             scrollToNearestVisibleCollectionViewCell()
         }
     }
+    func highLightCellAtIndexPath(indexPath:IndexPath){
+        songsCollectionView.scrollToItem(at: indexPath, at: .centeredHorizontally, animated: true)
+        for i in 0..<songsCollectionView.visibleCells.count {
+            let cell = songsCollectionView.visibleCells[i]
+            cell.layer.borderWidth=0.0
+            cell.alpha=0.3
+        }
+        let cell = songsCollectionView.cellForItem(at: indexPath)
+        cell?.layer.borderWidth = 2.0
+        cell?.layer.borderColor = UIColor.gray.cgColor
+        cell?.layer.cornerRadius=(cell?.layer.frame.width)!/2
+        cell?.alpha=1.0
+    }
     // MARK: Outlet Methods
     @IBAction func playAudioAtSliderValue(_ sender: Any) {
-        if (audioPlayer?.isPlaying)! {
-            audioPlayer!.currentTime=TimeInterval(progressIndicator.value)
-        }else if (audioPlayer!.prepareToPlay()){
-            audioPlayer!.play(atTime: TimeInterval(progressIndicator.value))
+        if audioPlayer?.rate == 0{
+            audioPlayer?.seek(to: CMTimeMakeWithSeconds(Float64(progressIndicator.value), 1))
             playButton.setImage(UIImage(named: "pauseIcon.png"), for: .normal)
             timer.invalidate()
             timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(self.updateCurrentTime), userInfo: nil, repeats: true)
-            
+        } else {
+            audioPlayer?.seek(to: CMTimeMakeWithSeconds(Float64(progressIndicator.value), 1))
         }
         
     }
     @IBAction func playButtonClick(_ sender: Any) {
-        if (audioPlayer?.isPlaying)! {
-            playButton.setImage(UIImage(named: "playIcon.png"), for: .normal)
-            audioPlayer!.pause()
-            timer.invalidate()
-        } else if (audioPlayer!.prepareToPlay()){
+        
+        if audioPlayer?.rate == 0{
             audioPlayer!.play()
             playButton.setImage(UIImage(named: "pauseIcon.png"), for: .normal)
             timer.invalidate()
             timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(self.updateCurrentTime), userInfo: nil, repeats: true)
+        } else {
+            audioPlayer!.pause()
+            playButton.setImage(UIImage(named: "playIcon.png"), for: .normal)
+            timer.invalidate()
         }
     }
+    // MARK: Helper Methods
     func updateViewTheme(themeStyle:String){
         let appDelegate = UIApplication.shared.delegate as! AppDelegate
         var vcBGColor:UIColor
@@ -268,6 +245,61 @@ class HomeViewController: UIViewController,AVAudioPlayerDelegate,UICollectionVie
         self.playerLayoutView.backgroundColor=vcBGColor
         self.songsCollectionView.backgroundColor=vcBGColor
         self.view.backgroundColor=vcBGColor
+    }
+    func updateCurrentTime(){
+        let currentCMTime:CMTime=(audioPlayer?.currentTime())!
+        let currentTime:TimeInterval=currentCMTime.seconds
+        currentTimeLabel.text=changeTimeIntervalToDisplayableString(time: currentTime)
+        progressIndicator.setValue(Float(currentTime), animated: false)
+    }
+    func changeTimeIntervalToDisplayableString(time:TimeInterval)->String{
+        var minutes = floor(time/60)
+        var seconds = Int(round(time - minutes * 60))
+        if(seconds==60){
+            seconds=0
+            minutes=minutes+1
+        }
+        let stringMinutes:String
+        let stringSeconds:String
+        if(Int(minutes)<10){
+            stringMinutes="0\(Int(minutes))"
+        }
+        else{
+            stringMinutes="\(Int(minutes))"
+        }
+        if(seconds<10){
+            stringSeconds="0\(seconds)"
+        }
+        else{
+            stringSeconds="\(seconds)"
+        }
+        return "\(stringMinutes):\(stringSeconds)"
+    }
+    func startMarqueeLabelAnimation(label:UILabel) {
+        
+        DispatchQueue.main.async(execute: {
+            
+            UILabel.animate(withDuration: 10.0, delay: 0.0, options: ([.curveEaseOut, .repeat]), animations: {() -> Void in
+                label.frame.origin.x-=200
+                
+            }, completion:  nil)
+        })
+    }
+    func setupAutoLayout(label:UILabel) {
+        let horizontalConstraintLeft = NSLayoutConstraint(item: label, attribute: NSLayoutAttribute.leftMargin, relatedBy: NSLayoutRelation.equal, toItem: playerLayoutView, attribute: NSLayoutAttribute.leftMargin, multiplier: 1, constant: 20)
+        let horizontalConstraintRight = NSLayoutConstraint(item: label, attribute: NSLayoutAttribute.rightMargin, relatedBy: NSLayoutRelation.equal, toItem: playerLayoutView, attribute: NSLayoutAttribute.rightMargin, multiplier: 1, constant: 20)
+        let verticalConstraint = NSLayoutConstraint(item: label, attribute: NSLayoutAttribute.topMargin, relatedBy: NSLayoutRelation.equal, toItem: playerLayoutView, attribute: NSLayoutAttribute.topMargin, multiplier: 1, constant: 20)
+        
+        self.playerLayoutView.addConstraints([horizontalConstraintLeft,horizontalConstraintRight, verticalConstraint])
+        
+    }
+    func finishedPlaying(myNotification:Notification) {
+        playButton.setImage(UIImage(named: "playIcon.png"), for: .normal)
+        timer.invalidate()
+        let stopedPlayerItem: AVPlayerItem = myNotification.object as! AVPlayerItem
+        stopedPlayerItem.seek(to:kCMTimeZero)
+        currentTimeLabel.text=changeTimeIntervalToDisplayableString(time: kCMTimeZero.seconds)
+        progressIndicator.setValue(Float(kCMTimeZero.seconds), animated: false)
     }
     
 }
